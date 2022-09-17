65f7a5b830dad776ae05a07e414c272c56f7fb56
trex.GUO <trexguojc@163.com>
2022-09-17 17:57:50 +0800

    One-Step

---
+++ src/main/java/com/trexguo/algorithm/MyAlgorithmDemo.java
@@ -0,0 +1,13 @@
+package com.trexguo.algorithm;
+
+/**
+ * @Author: GUO.jc
+ * @Date: 2022/9/17  17:51
+ * @Description: 我的第一个算法Demo
+ * @version: 1.0
+ */
+public class MyAlgorithmDemo {
+    public static void main(String[] args) {
+        System.out.println("Where there is a will, there is a way!");
+    }
+}
---
+++ src/main/java/com/trexguo/dataStructure/MyDataStructureDemo.java
@@ -0,0 +1,13 @@
+package com.trexguo.dataStructure;
+
+/**
+ * @Author: GUO.jc
+ * @Date: 2022/9/17  17:53
+ * @Description: 我的第一个数据结构Demo
+ * @version: 1.0
+ */
+public class MyDataStructureDemo {
+    public static void main(String[] args) {
+        System.out.println("There is nothing to lose, fight till the last moment!");
+    }
+}
